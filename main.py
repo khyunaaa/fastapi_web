f665fdf73bf0ec4ae89070a92fa9f9e0f1aade17
Your Name <you@example.com>
2024-02-01 14:20:48 +0900

    GET, POST, QUERY, FORM

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI,Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
@@ -10,6 +10,31 @@ async def main(request: Request):
      return templates.TemplateResponse(request=request, name="index.html", context={"name":"효나"})
 
 @app.get("/hello/{name}", response_class=HTMLResponse)
-async def hello(request: Request, name):
-    words = f'당신의 이름은 {name} 안농 반가워'
-    return templates.TemplateResponse(request=request, name="hello.html", context={"name":words})
+async def hello(request: Request, name, action, sound: str = "빵빵"):
+    print(f'action:{action} 그리고 소리 :{sound}')
+    return templates.TemplateResponse(request=request, 
+                                      name="hello.html", 
+                                      context={"name":name, 
+                                               "action":action, 
+                                               "sound":sound})
+@app.get('/login', response_class=HTMLResponse)
+async def login_page_view(request: Request):
+     return templates.TemplateResponse(request=request, name="login.html")
+
+@app.post('/login', response_class=HTMLResponse)
+async def login(username : str=Form(...), password: str=Form(...)):
+     print(username, password)
+     return "Success"
+
+@app.get('/register', response_class=HTMLResponse)
+async def register(request: Request):
+     return templates.TemplateResponse(request=request, name="register.html")
+
+@app.post('/register', response_class=HTMLResponse)
+async def login(username : str=Form(...), 
+               email: str=Form(...),
+               phone: str=Form(...),
+               password: str=Form(...)
+               ):
+     print(username, password)
+     return email
